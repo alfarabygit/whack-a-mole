c68caa22e3d7a644b5fedb9e0e2f292836faefe2
Alfaraby Indra <alfarabyindra@gmail.com>
2023-11-15 13:56:18 +0700

    reupload

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,7 @@ let groundBefore;
 let clear;
 let score;
 
+//fungsi utk tempat tikus muncul secara random
 function randomGround(ground) {
   const g = Math.floor(Math.random() * ground.length);
   const gRandom = ground[g];
@@ -18,10 +19,12 @@ function randomGround(ground) {
   return gRandom;
 }
 
+//fungsi utk waktu kemunculan tikus dari dalam tanah
 function randomTime(min, max) {
   return Math.round(Math.random() * (max - min) + min);
 }
 
+//fungsi utk menampilkan tikus dari dalam tanah
 function appearMole() {
   const gRandom = randomGround(ground);
   const tRandom = randomTime(300, 1000);
@@ -35,6 +38,7 @@ function appearMole() {
   }, tRandom);
 }
 
+//fungsi utk tombol memulai permainan
 function startPlay() {
   clear = false;
   score = 0;
@@ -45,6 +49,7 @@ function startPlay() {
   }, 5000);
 }
 
+//fungsi utk menambah skor ketika user memukul tikus
 function pow() {
   score++;
   this.parentNode.classList.remove("muncul");
